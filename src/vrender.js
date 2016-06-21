4586f3e15391d252d383cc869dd9928e3a48f621
fiatjaf <fiatjaf@gmail.com>
2016-06-21 10:28:16 -0400

    more styling tweaks.

--- src/vrender.js
+++ src/vrender.js
@@ -7,9 +7,47 @@ export function list (data) {
 }
 
 export function thread (data) {
-  return h('ul.thread', data.messages.slice(0, 3).map(m =>
-    h('li.message', m.text)
-  ))
+  let messages = data.messages.slice(0, 7)
+  let random = Math.random() * 7
+  var rotateLeft = Math.random() <= 0.5
+  var rotate = rotateLeft ? -random : random
+  var color = 255
+
+  return h('ul.thread', {
+    hook: {
+      insert (vnode) {
+        vnode.elm.style
+      }
+    }
+  }, messages.map((message, i) => {
+    color = color - i * 10
+
+    let v = h('li.message', {
+      style: {
+        'z-index': `${-i + 10}`,
+        'top': '2px',
+        'left': '2px',
+        'transform': `rotate(${parseInt(rotate)}deg)`,
+        'background-color': `rgb(${color}, ${color}, ${color})`,
+        'opacity': `${(10 - i) / 10}`,
+        'padding': `${parseInt(Math.random() * 6 + (i * 2 + 2))}px`
+      },
+      hook: {
+        insert (vnode) {
+          vnode.elm.style.top = `${(vnode.elm.parentNode.offsetHeight - vnode.elm.offsetHeight) / 2}px`
+        },
+        postpatch (_, vnode) {
+          vnode.elm.style.top = `${(vnode.elm.parentNode.offsetHeight - vnode.elm.offsetHeight) / 2}px`
+        }
+      }
+    }, message.text)
+
+    let random = Math.random() * 7
+    rotateLeft = !rotateLeft
+    rotate = (rotateLeft ? -random : random) % 360
+
+    return v
+  }))
 }
 
 export function empty () {
